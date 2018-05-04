d3540cddcdd79d6414261f348722ad8e4dec5d85
Marbelous <maksimikk@gmail.com>
2018-05-04 17:30:49 -0400

    mods to EnemyMotion.cs for code cleanup.

--- Assets/BoxEditor.cs
+++ Assets/BoxEditor.cs
@@ -4,20 +4,29 @@
 [SelectionBase]
 public class BoxEditor : MonoBehaviour
 {
-    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    const int gridSize = 10;
     Vector3 snapPos = new Vector3();
+
+    TextMesh posText;
 
     void Update()
     {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void UpdateLabel()
+    {
         TextMesh posText = GetComponentInChildren<TextMesh>();
-
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         string BoxPosText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
         posText.text = BoxPosText;
         gameObject.name = "Box: " + BoxPosText;
+    }
 
+    private void SnapToGrid()
+    {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
 }
