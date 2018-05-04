7c2eb00e83893926d7a71ef6ebf59e3c47711a6d
Marbelous <maksimikk@gmail.com>
2018-05-04 10:01:12 -0400

    Adding initial commit of Unity and C# files.

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    Vector3 snapPos = new Vector3();
+
+    void Update()
+    {
+        TextMesh posText = GetComponentInChildren<TextMesh>();
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        posText.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
